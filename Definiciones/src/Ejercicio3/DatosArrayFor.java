4d1a3d167d402d6d318b186e1bcd780e1aa4b8e0
Angelika <a.chozas.lopez@accenture.com>
2023-01-18 11:59:57 +0100

    Array en bucle for

---
+++ Definiciones/src/Ejercicio3/DatosArrayFor.java
@@ -0,0 +1,18 @@
+package Ejercicio3;
+
+public class DatosArrayFor {
+
+	public static void main(String[] args) {
+		int[][] diaMes = new int [5][7]; //Se inicializa el array con un tamaño
+		int contador = 0; //añadimos un contador 
+		//Se rellena el array con un ciclo for
+		for(int i=0; i < diaMes.length & contador < 31; i++) {
+			for(int b=0; b < diaMes[i].length & contador < 31; b++) {
+				diaMes[i][b] = ++contador;
+				System.out.println(diaMes[i][b]);
+			}
+		}
+		
+	}
+
+}
