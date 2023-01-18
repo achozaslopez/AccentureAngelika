66b82d0c728d574807914d34234937fd39468830
Angelika <a.chozas.lopez@accenture.com>
2023-01-18 12:02:01 +0100

    añado un espacio para mejorar la visualizacion

--- Definiciones/src/Ejercicio3/DatosArrayFor.java
+++ Definiciones/src/Ejercicio3/DatosArrayFor.java
@@ -4,7 +4,8 @@ public class DatosArrayFor {
 
 	public static void main(String[] args) {
 		int[][] diaMes = new int [5][7]; //Se inicializa el array con un tamaño
-		int contador = 0; //añadimos un contador 
+		int contador = 0; //Añadimos un contador 
+		
 		//Se rellena el array con un ciclo for
 		for(int i=0; i < diaMes.length & contador < 31; i++) {
 			for(int b=0; b < diaMes[i].length & contador < 31; b++) {
