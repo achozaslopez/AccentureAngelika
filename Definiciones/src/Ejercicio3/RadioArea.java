13fa7921aba0ccd2fb0ec3ec82bd0ab480cb1464
Angelika <a.chozas.lopez@accenture.com>
2023-01-18 11:05:35 +0100

    Ejercicio calcula el area del circulo y la longitud de la circunferencia

---
+++ Definiciones/src/Ejercicio3/RadioArea.java
@@ -0,0 +1,14 @@
+package Ejercicio3;
+
+public class RadioArea {
+
+	public static void main(String[] args) {
+		double r = 23;
+		double area = Math.PI * Math.pow(r, 2) ;
+		double longitud = 2 * Math.PI * r;
+		System.out.println("El área del circulo es" + area );
+		System.out.println("La longitud de la circunferencia es " + longitud);
+
+	}
+
+}
