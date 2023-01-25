fe10ecd3e73ab7c7f64be086670686bb47008d1a
Angelika <a.chozas.lopez@accenture.com>
2023-01-25 20:12:43 +0100

    Primer ejercicio

---
+++ EjerciciosRepasoExtra/src/ejercicio01/Ejercicio1.java
@@ -0,0 +1,23 @@
+package ejercicio01;
+/**
+ * 
+ * @author Angelika Chozas López
+ *
+ */
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		int numEntero = 5;
+		long numLargo = 1234141212;
+		double numDecimal = 34.65;
+		boolean verdadero = true;
+		String cadena = "Hola mundo";
+		
+		System.out.println("Número Entero: " + numEntero);
+		System.out.println("Número Long: " + numLargo);
+		System.out.println("Número Decimal: " + numDecimal);
+		System.out.println("Cadena de texto: " + cadena);
+
+	}
+
+}
